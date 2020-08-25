1b79de2a169c2766c2a2a33380e95ef0d3ec1620
hashito <hasitozzz@gmail.com>
2020-08-25 09:18:47 +0900

    bug fix

--- TweetRSS/main.py
+++ TweetRSS/main.py
@@ -22,9 +22,9 @@ def cash_read():
     with open(CASH_FILE) as f:
         return json.loads(f.read())
 
-def cash_wite(data):
-    with open(CASH_FILE,"w") as f:
-        f.wite(json.dumps(data),ensure_ascii=False)
+def cash_write(data):
+    with open(CASH_FILE,mode="w") as f:
+        f.write(json.dumps(data,ensure_ascii=False))
 
 def read_rss():
     return feedparser.parse(RSS_URL)
@@ -47,8 +47,8 @@ if(__name__ == '__main__'):
                 print("tweet:",f"{i['title']} {i['link']} {ADD_TEXT}")
                 send_tweet(f"{i['title']} {i['link']} {ADD_TEXT}")
                 time.sleep(TWEET_DELAY)
-            cash["id"]=tm
-            cash_wite(cash)
+            cash[i["id"]]=tm
+            cash_write(cash)
         for k in cash.keys():
             if((cash[k]+60)<tm):
                 print("delete:",k,cash[k],tm)
